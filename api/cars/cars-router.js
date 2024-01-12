a3d13a41979e73ddaf3f112885a196245b6c97cf
Tito2point0 <defconx00@gmail.com>
2024-01-11 19:29:10 -0500

    seeds made no db

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,33 @@
-// DO YOUR MAGIC
+const router = require("express").Router()
+const md = require("./cars-middleware")
+const Cars = require("./cars-model")
+
+router.get("/", async (req, res, next) => {
+    try {
+        const cars = await Cars.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/:id", md.checkCarId, async (req, res, next) => {
+    try {
+        res.json(req.car)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/",
+    md.checkCarPayload,
+    md.checkVinNumberValid,
+    md.checkVinNumberUnique,
+    async (req, res, next) => {
+    try {
+        const newCar = await Cars.create(req.body)
+        res.status(201).json(newCar)
+    } catch (err) {
+        next(err)
+    }
+})  
--- api/server.js
+++ api/server.js
@@ -2,6 +2,18 @@ const express = require("express")
 
 const server = express()
 
-// DO YOUR MAGIC
+// Fill out details for the following endpoints: cars, sales, sales/:id
+
+const carsRouter = require("./cars/cars-router")
+
+server.use(express.json())
+
+server.use("/api/cars", carsRouter)
+
+server.use('*', (req, res, next) => { 
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 
 module.exports = server
