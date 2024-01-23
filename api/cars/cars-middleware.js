38ac21748084118c31d6ca7ae3334cceed24a56f
Tito2point0 <defconx00@gmail.com>
2024-01-23 14:47:18 -0500

    vin validator

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,32 +1,66 @@
-const Car = require("./cars-model")
+const Car = require("./cars-model");
+const vinValidator = require("vin-validator");
 
 const checkCarId = async (req, res, next) => {
   try {
-    const cars = await Car.getById(req.params.id)
+    const cars = await Car.getById(req.params.id);
     if (!cars) {
-      next({ status: 404, message: `car with id ${req.params.id} is not found` })
+      next({
+        status: 404,
+        message: `car with id ${req.params.id} is not found`,
+      });
     } else {
-      req.car = cars
-      next()
+      req.car = cars;
+      next();
     }
-} catch (err) {
-    next(err)
+  } catch (err) {
+    next(err);
   }
-  
-}
+};
 
-const checkCarPayload = (req, res, next) => {
- next()
-}
+const checkCarPayload = async (req, res, next) => {
+  try {
+    const { vin, make, model, mileage } = req.body;
+    if (!vin) {
+      next({ status: 400, message: `vin is missing` });
+    } else if (!make) {
+      next({
+        status: 400,
+        message: `make is missing`
+      });
+    } else if (!model) {
+      next({
+        status: 400,
+        message: `model is missing`
+      });
+    } else if (!mileage) {
+      next({
+        status: 400,
+        message: `mileage is missing`
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
- next()
-}
+  if (vinValidator.validate(req.body.vin)) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: `vin ${req.body.vin} is invalid`
+    });
+  }
+};
 
 const checkVinNumberUnique = (req, res, next) => {
- next()
-}
- 
+  next();
+};
+
 module.exports = {
   checkCarId,
   checkCarPayload,
